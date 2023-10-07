58628ca7d0b6ddf88dc3f0a8aa76c1a9abcf67ca
YouX <56198358+1654366772@users.noreply.github.com>
2023-10-07 17:16:30 +0800

    Update index.js
    修改api

--- src/api/index.js
+++ src/api/index.js
@@ -30,7 +30,7 @@ axios.interceptors.response.use(function (response) {
 
 const base = {
   axios: axios,
-  baseUrl: 'https://openkey.cloud'
+  baseUrl: 'https://openkey.cloud/v1'
 }
 
 export default base
